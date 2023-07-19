7b3d5bbc859fcadf969219548663be02db259fc8
Bulbul Ahmed <116268132+bulbul-digigate360@users.noreply.github.com>
2023-07-19 14:54:14 +0600

    Update Image

---
+++ src/main/java/dailycodebuffer/command/api/aggregate/ProductAggregate.java
@@ -0,0 +1,17 @@
+package dailycodebuffer.command.api.aggregate;
+
+
+import org.axonframework.modelling.command.AggregateIdentifier;
+import org.axonframework.spring.stereotype.Aggregate;
+
+import java.math.BigDecimal;
+
+@Aggregate
+public class ProductAggregate {
+
+    @AggregateIdentifier
+    private String productId;
+    private String name;
+    private BigDecimal price;
+    private Integer quantity;
+}
